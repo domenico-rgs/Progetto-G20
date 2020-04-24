36a54da0a392b8b62691c37b5f03430e15fdc95f
killertag <killertag@gmail.com>
2020-04-24 13:41:49 +0200

    eliminazione classe tester per problemi e creazione un altra classe tester

--- src/it/unipv/www/g20/model/test/Tester.java
+++ src/it/unipv/www/g20/model/test/Tester.java
@@ -1,21 +1,12 @@
 package it.unipv.www.g20.model.test;
 
-import java.text.ParseException;
-
-import javax.swing.plaf.TextUI;
-
-import it.unipv.www.g20.model.TextUI.AdminTextUI;
 import it.unipv.www.g20.model.cinema.Cinema;
-import it.unipv.www.g20.model.exception.NotAvailableException;
-import it.unipv.www.g20.model.exception.SearchException;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		Cinema test = new Cinema("prova");
-		AdminTextUI c = new AdminTextUI(test);
+		Cinema cin1 = new Cinema("Uci");
 		
-		c.start();
 	}
 
 }
