23e898657d1ab1d9212c9b29d2b4cc08e2013c5d
killertag <killertag@gmail.com>
2020-04-24 14:26:39 +0200

    test falliti di inserimento controllare classe test per altri dettagli

--- src/it/unipv/www/g20/model/test/Tester.java
+++ src/it/unipv/www/g20/model/test/Tester.java
@@ -31,8 +31,14 @@ public class Tester {
 		}
 		try {
 			cin1.createMovieShowing("MrRobot", "sala1", "24/04/2020 20:30", 8.90);
-			cin1.createMovieShowing("JamesBond", "sala1", "24/04/2020 20:31", 8.90);  //primo errore la proiezione deve avere un lasso di tempo prestabilito
-			// cin1.createMovieShowing("MrRobot", "sala1", "24/04/2020 20:30", 8.90); //test ok aggiunta proiezione uguale
+			cin1.createMovieShowing("MrRobot", "sala1", "60/04/2020 10:30", 8.90);		//data errata ma viene accettata lo stesso
+			cin1.createMovieShowing("MrRobot", "sala1", "24/04/2020 60:30", 8.90);		//orario errato ma viene accettato lo stesso
+			cin1.createMovieShowing("MrRobot", "sala1", "24/04/2020 20:39", 90000.0);
+			//cin1.createMovieShowing("MrRobot", "sala1", "24-04-2020 20:30", 8.90);	//tipo di data non valido
+			cin1.createMovieShowing("JamesBond", "sala1", "24/04/2020 20:31", 8.90);  	//primo errore la proiezione deve avere un lasso di tempo prestabilito
+			// cin1.createMovieShowing("MrRobot", "sala1", "24/04/2020 20:30", 8.90); 	//test ok aggiunta proiezione uguale
+			//cin1.createMovieShowing("MrRobot2", "sala1", "24/04/2020 20:33", 8.90); 	//condizione in cui controlla l-esistenza di un film
+			//cin1.createMovieShowing("MrRobot", "sala2", "24/04/2020 20:30", 8.90);  	//condizione in cui si controlla l'esistenza della sala
 		} catch (ParseException | SearchException e) {
 			
 			e.printStackTrace();
