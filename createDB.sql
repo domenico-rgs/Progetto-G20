e010830e229152f90d51d9bc878286762ae6fe37
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2020-04-26 17:36:50 +0200

    aggiornato DB

--- createDB.sql
+++ createDB.sql
@@ -9,6 +9,17 @@ DROP TABLE IF EXISTS ticket;
 DROP TABLE IF EXISTS movieShowing;
 DROP TABLE IF EXISTS movie;
 
+CREATE TABLE movie
+        (title CHAR(10) PRIMARY KEY,
+         duration SMALLINT NOT NULL,
+         genere CHAR(10) ,
+         description VARCHAR(1000),
+	 pathMovieCover VARCHAR(100));
+     
+CREATE TABLE theatre
+        ( theatreName CHAR(10) PRIMARY KEY,
+          nRow SMALLINT,
+	  nCol SMALLINT);
 
 CREATE TABLE movieShowing
         (dateMovieShowing TIMESTAMP,
@@ -17,46 +28,31 @@ CREATE TABLE movieShowing
 	 movie CHAR(10) REFERENCES movie(title),
 
 	 PRIMARY KEY (dateMovieShowing, theatre),
-	 FOREIGN KEY (theatre) REFERENCES theatre(name),
-	 FOREIGN KEY (title) REFERENCES movie(title));
-
-CREATE TABLE theatre
-        ( name CHAR(10) PRIMARY KEY,
-          nRow SMALLINT,
-	  nCol SMALLINT);
+	 FOREIGN KEY (theatre) REFERENCES theatre(theatreName),
+	 FOREIGN KEY (movie) REFERENCES movie(title));
 
 CREATE TABLE seat
-        ( position CHAR(4),
+        ( pos CHAR(4),
           theatre CHAR(10),
          
-	  PRIMARY KEY (position,theatre),
-	  FOREIGN KEY (theatre) REFERENCES theatre(name));
+	  PRIMARY KEY (pos,theatre),
+	  FOREIGN KEY (theatre) REFERENCES theatre(theatreName));
 
 CREATE TABLE occupiedSeat
-        ( position CHAR(4),
+        ( pos CHAR(4),
           dateMovieShowing TIMESTAMP,
           theatre CHAR(10),
 
-	  PRIMARY KEY (position, dateMovieShowing),
-	  FOREIGN KEY (position) REFERENCES seat(seat),
-	  FOREIGN KEY (theatre) REFERENCES theatre(name),
+	  PRIMARY KEY (pos, dateMovieShowing, theatre),
+	  FOREIGN KEY (pos, theatre) REFERENCES seat(pos, theatre),
 	  FOREIGN KEY (dateMovieShowing) REFERENCES movieShowing(dateMovieShowing));
 
 CREATE TABLE ticket
-        (code CHAR(4) PRIMARY KEY,
+        (ticketCode CHAR(4) PRIMARY KEY,
          finalPrice DECIMAL (4,2) NOT NULL,
          occupiedSeat CHAR(4) NOT NULL,
 	 dateMovieShowing TIMESTAMP,
 	 theatre CHAR(10),
 
-         FOREIGN KEY (dateMovieShowing) REFERENCES occupiedSeat(dateMovieShowing),
-	 FOREIGN KEY (theatre) REFERENCES theatre(name),
-         FOREIGN KEY (occupiedSeat) REFERENCES occupiedSeat(position));
-
-CREATE TABLE movie
-        (title CHAR(10) PRIMARY KEY,
-         duration SMALLINT NOT NULL,
-         genere CHAR(10) ,
-         description VARCHAR(1000)
-	 pathMovieCover VARCHAR(100));
+         FOREIGN KEY (occupiedSeat, theatre, dateMovieShowing) REFERENCES occupiedSeat(pos, theatre, dateMovieShowing));
 
