e4b2e07bb95ed788e89d79ca91b4ece86025fd4d
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2020-04-30 20:50:12 +0200

    modificati colore e trasparenza navbar, banner e footer

--- webGUI/assets/js/navbar.js
+++ webGUI/assets/js/navbar.js
@@ -3,23 +3,27 @@ window.onscroll = function()
   scrollFunction()
 };
 
-var rgb = 'rgb(' + 51 + ',' + 51 + ',' + 51 + ',';
+var rgb = 'rgb(' + 30 + ',' + 63 + ',' + 97 + ',';
 var element = document.getElementById("header");
 
 
 // fa sicuramente schifo...lo sistemo in futuro
+//quando torna su non torna trasparente
+
+//magari si potrebbe lasciare la trasparenza a 0.75 e quando ci si passa sopra
+//col mouse farla diventare 1
 function scrollFunction() {
 
-element.style.backgroundColor = rgb + 0 + ')';
+element.style.backgroundColor = rgb + 0.75 + ')';
 
 
 switch (true) {
   case (document.documentElement.scrollTop > 380):
-      element.style.backgroundColor = rgb + 1 + ')';
+      element.style.backgroundColor = rgb + 0.65 + ')';
     break;
 
   case (document.documentElement.scrollTop > 190):
-        element.style.backgroundColor = rgb + 0.7 + ')';
+        element.style.backgroundColor = rgb + 0.60 + ')';
       break;
 
   case (document.documentElement.scrollTop > 130):
