5a958ec79b1773c60e2841e64cb1060ff561c4a5
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2020-07-14 20:27:16 +0200

    merge conflicts

--- src/main/java/server/handler/ShopCart.java
+++ src/main/java/server/handler/ShopCart.java
@@ -22,14 +22,9 @@ public class ShopCart implements IHandler {
 	private boolean valid;
 
 	private ShopCart() {}
-
-<<<<<<< HEAD:src/main/java/server/handler/ShopCart.java
-	public static ShopCart getInstance() {
-=======
 	
 	//*singleton*/
-	public static ShopCard getInstance() {
->>>>>>> 69a6d9a0aa57107eb8249fe922ab30414c731d1a:src/main/java/server/handler/ShopCard.java
+	public static ShopCart getInstance() {
 		if (instance == null) {
 			instance = new ShopCart();
 		}
