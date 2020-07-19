9d832b27fba48bda98fe70a507944cc0bb7190ca
killertag <killertag@gmail.com>
2020-07-19 17:55:56 +0200

    Junit tests created

---
+++ src/test/java/theatreTest/TheatreTest.java
@@ -0,0 +1,94 @@
+package theatreTest;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import server.domain.cinema.theatre.DisabledSeat;
+import server.domain.cinema.theatre.PremiumSeat;
+import server.domain.cinema.theatre.Seat;
+import server.domain.cinema.theatre.Theatre;
+import server.domain.exception.SeatException;
+import server.handler.Administrator;
+
+public class TheatreTest {
+
+	/**test of the instancing of the theater*/
+	@Test
+	public final void testTheatre() throws IOException, SeatException {
+		
+		Theatre theatre1 = new Theatre("theatreTest1");
+		
+		boolean testInstanced = true;	
+		
+		/**I use testInstanced boolean variable to check if it has not been instantiated, 
+		 * if so it will come out true*/
+		testInstanced = theatre1==null;
+	
+		/**if it is instantiated I expect the Boolean variable to be false*/
+		assertEquals(false, testInstanced);
+		
+		/**the test will be passed if it is coincident testIntanced == false*/
+		
+	}
+
+	
+	/**test adding seats*/
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public final void testCreateSeats() throws IOException, SeatException {
+		boolean testSeats = true;   	//variable that I use to verify the result that in theory I should get
+		Map<String, Seat> seatList = new HashMap<>();
+		
+		Theatre theatre2 = new Theatre("theatreTest2");    //instantiate a theater with the name "theaterTest"
+		 
+		String fileString = "src/main/resources/theatreConf/theatreTest.txt";
+		
+		theatre2.createSeats(fileString);
+		seatList = theatre2.getSeatsList();
+		
+		/**check if the hashmap is empty, if yes set testSeats to true*/
+		testSeats = seatList.isEmpty();
+		
+		/**check if the seats are among the possible acceptable combinations, 
+		 * default seat, premium seat or handicap seat*/
+		for (Map.Entry<String, Seat> entry : seatList.entrySet()) {
+			boolean testValue = false;
+			
+			testValue = entry.getValue().equals(Seat.class) || entry.getValue().equals(PremiumSeat.class) || entry.getValue().equals(DisabledSeat.class);
+			
+			if(testValue == true) {
+				testSeats = false;
+			}
+		 }
+		
+		assertEquals(false, testSeats);
+	}
+	
+	
+	
+	/**test if it creates the path and creates the file*/
+	@Test
+	public final void testCreateConfigFile() throws IOException, SeatException {
+		boolean testSeats = false;   	//variable that I use to verify the result that in theory I should get
+		
+		/**Random string similar to the one that will appear for the configuration of the seats*/
+		String config = "X X X P P P \nD D D X X X \n X X X X X X";    
+		String filePath = "";
+		Theatre theatre3 = new Theatre("theatreTest3");    //instantiate a theater with the name "theaterTest3"
+		
+		filePath = theatre3.createConfigFile(config);
+		
+		/**check given the following path just created there is the file just created*/
+		testSeats = new File(filePath).isFile();
+		
+		
+		assertEquals(true, testSeats);
+	}
+
+}
